06160619c05ead0fc5fde5325803f81121b94bed
Ali Ghorashi <aligho@mit.edu>
2022-02-06 13:51:49 -0500

    All Alkali Magnetizations

---
+++ Isolated_Atom_Magnetization/Cesium/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+##SBATCH -a 1-101
+#SBATCH -a 88
+#SBATCH -n 19
+#SBATCH -o Alkali-%a.out
+#SBATCH --exclusive
+
+source /etc/profile
+
+module load mpi/openmpi-4.0.5
+module load julia/1.6.1
+
+
+export dir=$(echo "print(split(pwd(), '/')[end])" | julia)
+
+export id=$SLURM_ARRAY_TASK_ID
+if [[ $dir == "Sodium" ]]; then
+	export alkali="Na"
+elif [[ $dir == "Potassium" ]]; then
+	export alkali="K"
+elif [[ $dir == "Lithium" ]]; then
+	export alkali="Li"
+elif [[ $dir == "Cesium" ]]; then
+	export alkali="Cs"
+elif [[ $dir == "Rubidium" ]]; then
+	export alkali="Rb"
+fi
+
+echo "The alkali metal is $alkali"
+
+#for scale in 2 3 4; do
+for scale in 4; do
+	export scale
+	echo "scale=$scale"
+	export magnetization=$(echo "print(round(-1+($id-1)*2/100, digits=3))" | julia)
+	echo "magnetization=$magnetization"
+	mpirun jdftx -i Alkali.in  | tee $alkali-$scale-$magnetization.out
+done
