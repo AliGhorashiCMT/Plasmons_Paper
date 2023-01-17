2ef733edad6e9ed4f0bf35ebe8cce80988c640b6
Ali Ghorashi <aligho@mit.edu>
2023-01-17 14:31:01 -0500

    taus BN22BC

--- Substitutional_Defects/BN22BC/Plasmons/run_tau_allen.sh
+++ Substitutional_Defects/BN22BC/Plasmons/run_tau_allen.sh
@@ -2,7 +2,8 @@
 #SBATCH -a 1-200
 #SBATCH -n 1
 #SBATCH -o tau_allen.out
-#SBATCH --exclusive
+#SBATCH --partition=xeon-g6-volta
+##SBATCH --exclusive
 module load julia/1.6.1
 export idx=$SLURM_ARRAY_TASK_ID
 julia run_tau_allen.jl $idx
