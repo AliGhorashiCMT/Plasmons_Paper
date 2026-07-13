ce94d77ac73f7c47037ed6484e2d2284ad3219e4
Ali Ghorashi <aligho@mit.edu>
2026-07-13 12:08:01 -0400

    K ground state

---
+++ Moire_Figs/Moire7_with_Alkali/Potassium/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -a 1-100
+#SBATCH -n 19
+#SBATCH -o Potassium-bands-%a.out
+#SBATCH --exclusive
+##SBATCH --partition=normal
+source /etc/profile
+module load julia/1.6.1
+module load mpi/openmpi-4.0.5
+
+export alkali=K
+export id=$SLURM_ARRAY_TASK_ID
+export d=$(echo "print(round(0.07+0.3*($id-1)/100, digits=4))" | julia)
+export d2=$(echo "print(round($d/2, digits=4))" | julia)
+echo "Second Layer Distance is $d"
+echo ""
+mpirun jdftx -i Moire.bands.in 
+
